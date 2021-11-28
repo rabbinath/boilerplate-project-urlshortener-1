f3a2747de8266b88f5da824efd21d4db16a6c9e3
Rabindra Nath <rabbinath@gmail.com>
2021-11-28 11:00:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,14 +25,39 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+let urlSchema=new mongoose.Schema({
+  orginal:{type:String, required:true},
+  short:Number
+})
+let Url=mongoose.model('Url',urlSchema)
+
+let url='mongodb+srv://rabbinath:'+process.env.PW+'@cluster0.0zmv9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 let resObj={};
-app.post('/api/shorturl',function(req,res){
-resObj['original_url']=req.body.originalUrl;
-resObj['short_url']=req.body.shorturl;
-
-res.json(resObj);
-
-});
+app.post('/api/shorturl',bodyParser.urlencoded({ extended: false }),function(req,res){
+resObj['original_url']=req.body['url'];
+let inputShort=1
+Url.findOne({})
+  .sort({short:'desc'})
+  .exec((error,result)=>{
+   if(!error && result!=undefinded){
+     inputShort=result.short+1
+   } 
+   if(!error){
+     Url.FindOneAndUpdate(
+       {original:inputUrl},
+       {original:inputUrl,short:inputShort},
+       {new:true,upsert:true},
+       (error,saveUrl)=>{
+         if(!error){
+           resObj['short_url']=saveUrl.short
+         }
+       }
+     )
+   }
+  }
+)
+})
 
 
 app.listen(port, function() {
